4ee9ce92ddd747a1f7d3698f749ff56292375605
Kağan Berke Maktav <controleng@yandex.com>
2023-11-15 11:24:29 +0300

    version get from yml

--- employee-service/src/main/java/com/aktog/yusuf/employeeservice/config/OpenApiConfig.java
+++ employee-service/src/main/java/com/aktog/yusuf/employeeservice/config/OpenApiConfig.java
@@ -4,16 +4,19 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @OpenAPIDefinition
 public class OpenApiConfig {
+    @Value("${employee-management-service.version}")
+    private String version;
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
-                .info(new Info().title("Employee Management System").version("1.0.0"))
+                .info(new Info().title("Employee Management System").version(version))
                 .externalDocs(new ExternalDocumentation()
                         .description("Documentation of Employee Management System")
                         .url("https://employee-management-system.com"));
